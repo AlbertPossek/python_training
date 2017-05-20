995ee1f98cc98d67057f48c357866a0586117d21
AlbertPossek <albert.possek@gmail.com>
2017-05-20 14:12:11 +0300

    Comments added

--- login4.py
+++ login4.py
@@ -19,21 +19,30 @@ class login4(unittest.TestCase):
         success = True
         wd = self.wd
         tmp = True
-        wd.get("http://localhost:8080/php4dvd/")    #Go to URL
+        # Open home page
+        wd.get("http://localhost:8080/php4dvd/")
+        # Find username field, clear it and set value "admin"
         wd.find_element_by_id("username").click()
         wd.find_element_by_id("username").clear()
         wd.find_element_by_id("username").send_keys("admin")
+        # Find password field, clear it and set value "admin"
         wd.find_element_by_id("password").click()
         wd.find_element_by_id("password").clear()
         wd.find_element_by_id("password").send_keys("admin")
+        # Select chackbox "remember me"
         wd.find_element_by_id("rememberme").click()         #select the checkbox remember me
+        # Check checkbox "remember me and print "Checkbox checked" in case it checked, or "Checkbox uchecked"  if it unchecked
         if wd.find_element_by_id("rememberme").is_selected():
-            print ("Checkbox checked")  # If remember me checked print "Checkbox unchecked"
+            print ("Checkbox checked")
         else:
-            print("Checkbox unchecked") # If remember me not checked print "Checkbox unchecked"
+            print("Checkbox unchecked")
+        # Click on "Logon button"
         wd.find_element_by_xpath("//div[@class='col-xs-4']//button[.='Log in']").click()
+        # Don't understand what is this and why tests failed without this line
         ActionChains(wd).move_to_element(wd.find_element_by_xpath("//header[@class='main-header']/nav/div[3]/ul/li[1]/a")).perform()
+        # Click on "Settings" icon
         wd.find_element_by_css_selector("a.dropdown-toggle").click()
+        # Click on "Logout" option
         wd.find_element_by_link_text("Log out").click()
         self.assertTrue(success)
     
