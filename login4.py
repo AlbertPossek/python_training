bb5c5091ad59be6f6b746796094ed6579df3bd58
AlbertPossek <albert.possek@gmail.com>
2017-05-20 16:07:02 +0300

    empty login added

--- login4.py
+++ login4.py
@@ -14,38 +14,69 @@ class login4(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
-    
-    def test_(self):
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost:8080/php4dvd/")
+
+    def set_username_password(self, wd, username, password):
+        wd.find_element_by_id("username").click()
+        wd.find_element_by_id("username").clear()
+        wd.find_element_by_id("username").send_keys(username)
+        wd.find_element_by_id("password").click()
+        wd.find_element_by_id("password").clear()
+        wd.find_element_by_id("password").send_keys(password)
+
+
+    def select_checkbox(self, wd):
+        # Select checkbox "remember me"
+        wd.find_element_by_id("rememberme").click()  # select the checkbox remember me
+
+    def print_checkbox_state(self, wd):
+        # Check checkbox "remember me and print "Checkbox checked" in case it checked, or "Checkbox uchecked"  if it unchecked
+        if wd.find_element_by_id("rememberme").is_selected():
+            print("Checkbox checked")
+        else:
+            print("Checkbox unchecked")
+
+    def click_login_button(self, wd):
+        wd.find_element_by_xpath("//div[@class='col-xs-4']//button[.='Log in']").click()
+
+    def dont_know_what_is_this(self, wd):
+        # Don't understand what is this and why tests failed without this line
+        ActionChains(wd).move_to_element(
+            wd.find_element_by_xpath("//header[@class='main-header']/nav/div[3]/ul/li[1]/a")).perform()
+
+    def click_on_settings_icon(self, wd):
+        wd.find_element_by_css_selector("a.dropdown-toggle").click()
+
+    def click_on_logout_option(self, wd):
+        wd.find_element_by_link_text("Log out").click()
+
+    def test_login_valid(self):
         success = True
         wd = self.wd
         tmp = True
-        # Open home page
-        wd.get("http://localhost:8080/php4dvd/")
-        # Find username field, clear it and set value "admin"
-        wd.find_element_by_id("username").click()
-        wd.find_element_by_id("username").clear()
-        wd.find_element_by_id("username").send_keys("admin")
-        # Find password field, clear it and set value "admin"
-        wd.find_element_by_id("password").click()
-        wd.find_element_by_id("password").clear()
-        wd.find_element_by_id("password").send_keys("admin")
-        # Select chackbox "remember me"
-        wd.find_element_by_id("rememberme").click()         #select the checkbox remember me
-        # Check checkbox "remember me and print "Checkbox checked" in case it checked, or "Checkbox uchecked"  if it unchecked
-        if wd.find_element_by_id("rememberme").is_selected():
-            print ("Checkbox checked")
-        else:
-            print("Checkbox unchecked")
-        # Click on "Logon button"
-        wd.find_element_by_xpath("//div[@class='col-xs-4']//button[.='Log in']").click()
-        # Don't understand what is this and why tests failed without this line
-        ActionChains(wd).move_to_element(wd.find_element_by_xpath("//header[@class='main-header']/nav/div[3]/ul/li[1]/a")).perform()
-        # Click on "Settings" icon
-        wd.find_element_by_css_selector("a.dropdown-toggle").click()
-        # Click on "Logout" option
-        wd.find_element_by_link_text("Log out").click()
+        self.open_home_page(wd)
+        self.set_username_password(wd, username="admin", password="admin")
+        self.select_checkbox(wd)
+        self.print_checkbox_state(wd)
+        self.click_login_button(wd)
+        self.dont_know_what_is_this(wd)
+        self.click_on_settings_icon(wd)
+        self.click_on_logout_option(wd)
         self.assertTrue(success)
-    
+
+    def test_login_empty(self):
+        success = True
+        wd = self.wd
+        tmp = True
+        self.open_home_page(wd)
+        self.set_username_password(wd, username="", password="")
+        self.select_checkbox(wd)
+        self.print_checkbox_state(wd)
+        self.click_login_button(wd)
+        self.assertTrue(success)
+
     def tearDown(self):
         self.wd.quit()
 
